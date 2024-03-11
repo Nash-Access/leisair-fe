b3d8b47b0ebd8d2ee2edc95bee869e32b45e19d2
Ayman Elhalwagy <66922109+ayyman-e@users.noreply.github.com>
2024-03-11 13:33:52 +0000

    updated with brunel logo

--- src/containers/Menu.tsx
+++ src/containers/Menu.tsx
@@ -49,7 +49,11 @@ const Menu = ({ menuList }: MenuProps) => {
     return (
         <nav className="flex items-center justify-between flex-wrap bg-blue-900 p-6 text-white">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
-                <Image className='pr-4' src="/img/nash-logo-light.svg" width="100" height="50" alt="LOGO HERE" />
+                <Image className='pr-4 my-4' src="/img/nash-logo-light.svg" width="90" height="50" alt="LOGO HERE" priority />
+                <svg className='fill-white' width={16} viewBox="0 0 384 512">
+                    <path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/>
+                </svg>
+                <Image className='pr-4' src="/img/brunel-logo.png" width="200" height="100" alt="LOGO HERE" priority />
                 <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto gap-6 px-6">
                     {menuList.map((item) => (
                         <a key={item.name} href={item.href} className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 font-bold text-3xl hover:text-white mr-4">
